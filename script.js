42715d38b8dae2c38c03b031e386334470c948f5
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-05-03 23:18:59 -0300

    adding a Lauch Again button and others improvements

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const starsTd = document.querySelectorAll('.star');
 
+//getting a random star index:
 function randomTd() {
   const randomIndex = Math.floor(Math.random() * starsTd.length);
   return randomIndex;
 }
 
+//removing the star animation:
 let saveArray = [];
-
 function starsDisappearing() {
   for (let i = 0; i < saveArray.length; i += 1) {
     starsTd[saveArray[i]].className = 'star';
@@ -14,11 +15,10 @@ function starsDisappearing() {
   saveArray = [];
 }
 
+//adding a animation to random stars and calling that function every 3s:
 setInterval(function () {
   for (let i = 0; i < 30; i += 1) {
     const rIndex = randomTd();
-    // let rDelay = randomDelay();
-    // starsTd[rIndex].style.animationDuration= `${rDelay}s`;
     starsTd[rIndex].classList.add('starsFlashing');
     saveArray.push(rIndex);
     starsTd[rIndex].addEventListener('animationend', starsDisappearing);
@@ -26,6 +26,40 @@ setInterval(function () {
   return saveArray;
 }, 3000);
 
+//reseting animations on click!
+const resetButton = document.getElementById('reset-button');
+const rocket = document.querySelector('.rocket-complete');
+const earth = document.querySelector('.earth');
+const sky = document.querySelector('.sky');
+const planets = document.querySelector('.planet-main');
+
+function resetAnimation(element) {
+  element.style.animation = 'none';
+  element.offsetHeight; /* trigger reflow */
+  element.style.animation = null;  
+}
+
+resetButton.addEventListener('click', ()=> {
+  resetAnimation(rocket);
+  resetAnimation(earth);
+  resetAnimation(sky);
+  resetAnimation(planets);
+  resetButton.style.display = 'none';
+})
+
+let animationCount = 0;
+rocket.addEventListener('animationend', ()=> {
+  console.log(animationCount);
+  animationCount += 1;
+  if (animationCount != 1) {
+    resetButton.style.display = 'block';
+    animationCount = 0;
+    console.log(animationCount);
+  }  
+  console.log(animationCount);
+})
+
+//mandatory step
 const articleDiv = document.querySelector('article');
 
 articleDiv.addEventListener('click', function () {
